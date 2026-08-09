e78823c413f956b931024269ffe4bf66d0d4a44f
WinG-E00 <gastonencinawj@gmail.com>
2026-08-09 07:38:06 -0300

    develop:Configuracion de la base de detos

---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { Sequelize } from "sequelize";
+
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+          dialect: 'mysql',
+          logging: false,
+      })
+    : new Sequelize(
+          process.env.DATABASE_NAME,
+          process.env.DATABASE_USER,
+          process.env.DATABASE_PASSWORD,
+          {
+              host: process.env.DATABASE_HOST,
+              dialect: 'mysql',
+              port: Number(process.env.DATABASE_PORT),
+              logging: false,
+          }
+      );
+
+export default sequelize;
+
+
