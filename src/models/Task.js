094f22fded0ba420c4cd6bce4278cff6fbc8baae
WinG-E00 <gastonencinawj@gmail.com>
2026-08-09 07:45:46 -0300

    develop:Configuracion del modelo de la tabla tasks

---
+++ src/models/Task.js
@@ -0,0 +1,40 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js'; 
+
+const Task = sequelize.define('Task', {
+
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+            notEmpty: true,
+            len: [1]
+        }
+    },
+
+    description: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [1]
+        }
+    },
+
+    isComplete: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        validate: {
+            isBoolean: true
+        }
+    }
+});
+
+export default Task;
